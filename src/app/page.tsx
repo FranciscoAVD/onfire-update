68de1aedeeca07098347ef5b675468a7ceed203e
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-07 20:58:47 -0400

    added nav links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <>
+      <header>
+        <nav className="flex items-center gap-4">
+          <Link href="/register">Register</Link>
+          <Link href="/login">Login</Link>
+          <Link href="/dashboard">Dashboard</Link>
+        </nav>
+      </header>
+      <main>Home page</main>
+    </>
+  );
 }
