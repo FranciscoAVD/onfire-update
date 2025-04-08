b125d18b727466ff5606959b2284e5050c178cf4
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-08 00:01:19 -0400

    refactored sidebar to fetch user session data in parent

---
+++ src/components/layout/sidebar/sidebar-nav.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import {
+  Home,
+  ShoppingBag,
+  Users,
+  CalendarDays,
+  Search,
+  CalendarClock,
+  LineChart,
+  Settings,
+} from "lucide-react";
+import { routes } from "@/lib/constants";
+import { Roles } from "@/features/users/lib/types";
+import { usePathname } from "next/navigation";
+
+const nav = [
+  {
+    route: routes.dashboard.index,
+    Icon: Home,
+  },
+  {
+    route: routes.dashboard.packages,
+    Icon: ShoppingBag,
+  },
+  {
+    route: routes.dashboard.groups,
+    Icon: Users,
+  },
+  {
+    route: routes.dashboard.staffSchedule,
+    Icon: CalendarDays,
+  },
+  {
+    route: routes.dashboard.search,
+    Icon: Search,
+  },
+  {
+    route: routes.dashboard.assignSchedule,
+    Icon: CalendarClock,
+  },
+  {
+    route: routes.dashboard.analytics,
+    Icon: LineChart,
+  },
+  {
+    route: routes.dashboard.settings,
+    Icon: Settings,
+  },
+] as const;
+
+export function SidebarNav({ role }: { role: Roles }) {
+  const path = usePathname();
+  return (
+    <nav className="space-y-1">
+      {nav.map(({ route, Icon }) => {
+        if (route.visibleTo.includes(role))
+          return (
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                "flex items-center gap-2 px-3 py-2 hover:bg-neutral-100 text-sm rounded-md capitalize transition-colors",
+                path === route.href &&
+                  "bg-primary text-primary-foreground hover:bg-primary/90"
+              )}
+            >
+              <Icon className="size-4" aria-hidden />
+              {route.label}
+            </Link>
+          );
+        else return null;
+      })}
+    </nav>
+  );
+}
--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -1,73 +1,12 @@
-"use client";
-
-import { usePathname } from "next/navigation";
+import { getCurrentUser } from "@/features/auth/lib/session";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
-import {
-  Home,
-  ShoppingBag,
-  Users,
-  CalendarDays,
-  Search,
-  CalendarClock,
-  LineChart,
-  Settings,
-} from "lucide-react";
-import { routes } from "@/lib/constants";
+import { SidebarNav } from "./sidebar-nav";
 
-const nav = [
-  {
-    route: routes.dashboard.index,
-    Icon: Home,
-  },
-  {
-    route: routes.dashboard.packages,
-    Icon: ShoppingBag,
-  },
-  {
-    route: routes.dashboard.groups,
-    Icon: Users,
-  },
-  {
-    route: routes.dashboard.staffSchedule,
-    Icon: CalendarDays,
-  },
-  {
-    route: routes.dashboard.search,
-    Icon: Search,
-  },
-  {
-    route: routes.dashboard.assignSchedule,
-    Icon: CalendarClock,
-  },
-  {
-    route: routes.dashboard.analytics,
-    Icon: LineChart,
-  },
-  {
-    route: routes.dashboard.settings,
-    Icon: Settings,
-  },
-] as const;
-
-export function Sidebar({ className }: { className?: string }) {
-  const path = usePathname();
+export async function Sidebar({ className }: { className?: string }) {
+  const user = await getCurrentUser();
   return (
     <aside className={cn(`py-4 px-2 ${className}`)}>
-      <nav className="space-y-1">
-        {nav.map(({ route, Icon }) => (
-          <Link
-            key={route.href}
-            href={route.href}
-            className={cn("flex items-center gap-2 px-3 py-2 hover:bg-neutral-100 text-sm rounded-md capitalize transition-colors",
-                path === route.href && "bg-primary text-primary-foreground hover:bg-primary/90"
-            )}
-          >
-            <Icon className="size-4" aria-hidden />
-            {route.label}
-          </Link>
-        ))}
-      </nav>
+        {user && <SidebarNav role={user.getRole()}/>}
     </aside>
   );
 }
