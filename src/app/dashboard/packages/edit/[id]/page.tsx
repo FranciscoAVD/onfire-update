88ef3cc017c33981340c79d419b579d535cfc57b
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-09 18:58:21 -0400

    edit package page

--- src/app/dashboard/packages/edit/[id]/page.tsx
+++ src/app/dashboard/packages/edit/[id]/page.tsx
@@ -3,9 +3,9 @@ import { Main } from "@/components/ui/main";
 import { getCurrentUser } from "@/features/auth/lib/session";
 import { getPackage } from "@/features/packages/use-cases/get-packages";
 import { ArrowLeft, Trash2 } from "lucide-react";
-import { H1, H2 } from "@/components/ui/headers";
+import { H1 } from "@/components/ui/headers";
 import { routes } from "@/lib/constants";
-import { EditPackageForm } from "@/features/packages/components/edit-package-form";
+import { EditPackageForms } from "@/features/packages/components/edit-package-forms";
 import { Button } from "@/components/ui/button";
 
 export default async function EditPackagePage(props: {
@@ -17,31 +17,29 @@ export default async function EditPackagePage(props: {
   const p = await getPackage(+id);
   return (
     <Main>
-      <section className="space-y-10">
-        <div>
-          <Link
-            href={routes.dashboard.packages.href}
-            className="flex items-center gap-2 mb-3"
-          >
-            <ArrowLeft className="size-4" aria-hidden />
-            Back to packages
-          </Link>
-          <H1>Edit an existing package</H1>
-          <p className="text-sm text-neutral-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            omnis earum officiis reiciendis molestias? Ea.
-          </p>
-        </div>
-        {p && (
-          <>
-            <EditPackageForm p={p} className="max-w-xl" />
-            <Button variant={"destructive"}>
-              <Trash2 aria-hidden />
-              Delete package
-            </Button>
-          </>
-        )}
+      <section>
+        <Link
+          href={routes.dashboard.packages.href}
+          className="flex items-center gap-2 mb-3"
+        >
+          <ArrowLeft className="size-4" aria-hidden />
+          Back to packages
+        </Link>
+        <H1>
+          Edit package
+          <br />
+          <span className="capitalize">
+            &quot;{p?.packageName}
+            &quot;
+          </span>
+        </H1>
+        <p className="max-w-lg text-sm text-neutral-500">
+          Altering the cost of a package might affect the accuracy of the
+          analysis when creating visuals for analytics. It is recommended to
+          create a new package for special events or seasons.
+        </p>
       </section>
+      {p && <EditPackageForms p={p} />}
     </Main>
   );
 }
