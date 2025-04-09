bb3f722dedb5d3482793a59e132b9670b6ef1a91
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-09 18:57:23 -0400

    schemas for update package forms

--- src/features/packages/lib/schemas.ts
+++ src/features/packages/lib/schemas.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 
 export const addPackageSchema = z
   .object({
-    name: z.string().min(1, "Package name is required."),
+    name: z
+      .string()
+      .min(1, "Package name is required.")
+      .max(50, "Package name is too long."),
     privates: z.string().min(1),
     cost: z.string().min(1, "Cost is required."),
     discount: z.string().min(1),
@@ -22,6 +25,45 @@ export const addPackageSchema = z
     message: "Invalid discount.",
   });
 
-export const updatePackageStatusSchema = z.object({
+export const updatePackageStatusSchema = z
+  .object({
+    id: z.string().min(1, "Invalid id."),
+    active: z.enum(["true", "false"]),
+  })
+  .refine((d) => Number.isInteger(+d.id));
+
+export const updatePackageNameSchema = z.object({
   id: z.string().min(1, "Invalid id."),
-}).refine(d => Number.isInteger(+d.id))
+  name: z
+    .string()
+    .min(1, "Package name is required.")
+    .max(50, "Package name is too long."),
+});
+
+export const updatePackageDescriptionSchema = z.object({
+  id: z.string().min(1, "Invalid id."),
+  description: z
+    .string()
+    .min(1, "Package description is required.")
+    .max(255, "Package description is too long."),
+});
+
+export const updatePackageNumbersSchema = z
+  .object({
+    id: z.string().min(1, "Invalid id."),
+    privates: z.string().min(1),
+    cost: z.string().min(1, "Cost is required."),
+    discount: z.string().min(1),
+  })
+  .refine((d) => Number.isInteger(+d.privates), {
+    path: ["privates"],
+    message: "Invalid number of privates.",
+  })
+  .refine((d) => Number.isInteger(+d.cost) && +d.cost >= MINIMUM_PRIVATE_COST, {
+    path: ["cost"],
+    message: "Invalid cost.",
+  })
+  .refine((d) => Number.isInteger(+d.discount), {
+    path: ["discount"],
+    message: "Invalid discount.",
+  });
