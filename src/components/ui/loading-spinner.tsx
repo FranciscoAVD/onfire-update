24820317ed12656a45bdb015745b06f1e565eba9
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-09 18:56:49 -0400

    made border thicker

--- src/components/ui/loading-spinner.tsx
+++ src/components/ui/loading-spinner.tsx
@@ -1,6 +1,11 @@
 import { cn } from "@/lib/utils";
 
-export function LoadingSpinner({className}:{className?: string}){
-
-    return <div className={cn(`size-5 border-2 border-t-neutral-500 rounded-full animate-spin ${className}`)} />
+export function LoadingSpinner({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        `size-5 border-4 border-t-neutral-500 rounded-full animate-spin ${className}`,
+      )}
+    />
+  );
 }
