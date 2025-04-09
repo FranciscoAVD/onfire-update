95ebf4369b5eb682fe84d3ca74c0a5198a133c68
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-09 18:57:54 -0400

    added update utils for package fields

--- src/features/packages/use-cases/update-package.ts
+++ src/features/packages/use-cases/update-package.ts
@@ -16,3 +16,50 @@ export async function updatePackageActivity(
     throw new Error("Database error while update package isActive.");
   }
 }
+
+export async function updatePackageName(
+  packageId: Package["id"],
+  newName: string,
+): Promise<void> {
+  try {
+    await db
+      .update(packageTable)
+      .set({ packageName: newName })
+      .where(eq(packageTable.id, packageId));
+  } catch (err) {
+    throw new Error("Database error while update package name.");
+  }
+}
+
+export async function updatePackageDescription(
+  packageId: Package["id"],
+  newDescription: string,
+): Promise<void> {
+  try {
+    await db
+      .update(packageTable)
+      .set({ description: newDescription })
+      .where(eq(packageTable.id, packageId));
+  } catch (err) {
+    throw new Error("Database error while update package description.");
+  }
+}
+
+/**
+@param cost - The price of the package in cents.
+*/
+export async function updatePackageNumbers(
+  packageId: Package["id"],
+  cost: number,
+  discount: number,
+  privates: number,
+): Promise<void> {
+  try {
+    await db
+      .update(packageTable)
+      .set({ numberOfPrivates: privates, discount: discount, cost: cost })
+      .where(eq(packageTable.id, packageId));
+  } catch (err) {
+    throw new Error("Database error while update package description.");
+  }
+}
