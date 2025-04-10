78aa9d32acd56e585e849543a2d9641ec7a09973
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-09 23:03:37 -0400

    added company title to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,14 @@
 import { UserButton } from "@/features/users/components/user-button";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export function Header({ className }: { className?: string }) {
   return (
     <header className={cn(`flex items-center ${className}`)}>
       <div className="flex items-center container mx-auto">
+        <Link href="/" className="text-xl font-bold">
+          On Fire Dance Studio
+        </Link>{" "}
         <UserButton className="ml-auto" />
       </div>
     </header>
