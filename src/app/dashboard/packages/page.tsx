bad4fdacfcf7086e59ac5b80e89711e04cc03bac
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-09 23:03:20 -0400

    added text to page

--- src/app/dashboard/packages/page.tsx
+++ src/app/dashboard/packages/page.tsx
@@ -1,15 +1,15 @@
 import { H1 } from "@/components/ui/headers";
 import { getCurrentUser } from "@/features/auth/lib/session";
 import { Main } from "@/components/ui/main";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-import Link from "next/link";
 import {
   getActivePackages,
   getAllPackages,
 } from "@/features/packages/use-cases/get-packages";
 import PackageCard from "@/features/packages/components/package-card";
 import { Package } from "@/database/types";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 export default async function PackagesPage() {
   const user = await getCurrentUser();
@@ -23,20 +23,22 @@ export default async function PackagesPage() {
   return (
     <Main>
       {user.isAdmin() && (
-        <Button size="icon" className="flex ml-auto" asChild>
+        <Button className="flex ml-auto w-fit" asChild>
           <Link href="/dashboard/packages/create">
             <Plus />
-            <span className="sr-only">Add package</span>
+            Add package
           </Link>
         </Button>
       )}
       <section className="space-y-10">
-        <div>
+        <div className="text-center">
           <H1>Nuestros paquetes</H1>
-          <p className="text-sm text-neutral-500">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos
-            dolorem, libero obcaecati voluptate tempora labore quam maxime
-            accusantium architecto dignissimos.
+          <p className="max-w-2xl mx-auto text-neutral-500">
+            Cuando compres cualquiera de nuestros paquetes podras reservar
+            clases privadas en cualquier ritmo que ofrezcamos. Cada paquete
+            tiene una fecha de expiracion y por lo tanto la totalidad de las
+            clases privadas tendran que programarse en o antes de la fecha de
+            expiracion.
           </p>
         </div>
         <div role="grid" className="grid lg:grid-cols-2 xl:grid-cols-3 gap-6">
