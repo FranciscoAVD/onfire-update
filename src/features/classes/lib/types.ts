73eeb920f97735429c833562406a12504d473730
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-14 18:23:45 -0400

    added types infered from zod schemas for style and rhythm

--- src/features/classes/lib/types.ts
+++ src/features/classes/lib/types.ts
@@ -1,1 +1,7 @@
+import { z } from "zod";
+import { rhythmSchema, styleSchema } from "./schemas";
+
 export type Times = Map<number, string>;
+
+export type Style = z.infer<typeof styleSchema>;
+export type Rhythm = z.infer<typeof rhythmSchema>;
