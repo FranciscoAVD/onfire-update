87eb67a3e61dadd30d4460aeee077a2ee4f6a0cb
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-15 09:17:45 -0400

    removed normalizing the data from the schema and updated previous to return string|string[]

--- src/features/auth/actions/register-action.ts
+++ src/features/auth/actions/register-action.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import { TResponse } from "@/lib/types"; 
+import { TResponse } from "@/lib/types";
 import { registerUserSchema } from "@/features/auth/lib/schemas";
 import { createSession, getSession } from "@/features/auth/lib/session";
-import { cleanPhoneNumber, formDataToObject } from "@/lib/utils";
+import { formDataToObject } from "@/lib/utils";
 import { isUser } from "@/features/users/use-cases/get-user";
 import { hashPassword } from "@/features/auth/lib/hash";
 import { addUser } from "@/features/users/use-cases/add-user";
@@ -18,12 +18,12 @@ interface RegisterResponse extends TResponse {
     phone?: string[] | undefined;
     password?: string[] | undefined;
   };
-  previous?: Record<string, string>
+  previous?: Record<string, string | string[]>;
 }
 
 export async function registerAction(
   previous: RegisterResponse,
-  unverified: FormData
+  unverified: FormData,
 ): Promise<RegisterResponse> {
   const session = await getSession();
   if (session) return { success: false };
@@ -31,31 +31,38 @@ export async function registerAction(
   const object = formDataToObject(unverified);
 
   const { success, data, error } = registerUserSchema.safeParse(object);
-  if (!success) return { success: false, errors: error.flatten().fieldErrors, previous: object };
- 
-  const normalizedEmail = data.email.toLowerCase();
+  if (!success)
+    return {
+      success: false,
+      errors: error.flatten().fieldErrors,
+      previous: object,
+    };
 
   //Ensure entity is not already a user.
-  const isRegistered = await isUser(normalizedEmail);
+  const isRegistered = await isUser(data.email);
   if (isRegistered)
-    return { success: false, errors: { email: ["Email already in use."] }, previous: object };
-  
+    return {
+      success: false,
+      errors: { email: ["Email already in use."] },
+      previous: object,
+    };
+
   //Register user
   const hash = await hashPassword(data.password);
 
   const userId = await addUser({
-    firstName: data.first.toLowerCase(),
-    lastName: data.last.toLowerCase(),
-    email: normalizedEmail,
-    phone: cleanPhoneNumber(data.phone),
+    firstName: data.first,
+    lastName: data.last,
+    email: data.email,
+    phone: data.phone,
     password: hash,
     role: DEFAULT_USER_ROLE,
   });
 
   await createSession({
     id: userId,
-    name: `${data.first} ${data.last}`.toLowerCase(),
-    email: normalizedEmail,
+    name: `${data.first} ${data.last}`,
+    email: data.email,
     role: DEFAULT_USER_ROLE,
   });
 
