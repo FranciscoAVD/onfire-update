ea86ed4021643bd38a2d0631d1ece6e1d01933ed
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-15 09:16:49 -0400

    modified register schema to normalize fields

--- src/features/auth/lib/schemas.ts
+++ src/features/auth/lib/schemas.ts
@@ -1,18 +1,27 @@
-import { isValidPhoneNumber } from "@/lib/utils";
+import { cleanPhoneNumber, isValidPhoneNumber } from "@/lib/utils";
 import { z } from "zod";
 
 export const registerUserSchema = z.object({
-  first: z.string().min(1, "First name is required."),
-  last: z.string().min(1, "Last name is required."),
-  email: z.string().email("Invalid email address."),
-  phone: z.string().min(1, "Phone number is required."),
-  password: z.string().min(8, "Password must contain at least 8 characters.")
-}).refine(d => isValidPhoneNumber(d.phone), {
-  path: ["phone"],
-  message: "Invalid phone number.",
+  first: z
+    .string()
+    .min(1, "First name is required.")
+    .transform((f) => f.toLowerCase()),
+  last: z
+    .string()
+    .min(1, "Last name is required.")
+    .transform((l) => l.toLowerCase()),
+  email: z
+    .string()
+    .email("Invalid email address.")
+    .transform((e) => e.toLowerCase()),
+  phone: z
+    .string()
+    .min(1, "Phone number is required.")
+    .refine((p) => isValidPhoneNumber(p), "Invalid phone number.")
+    .transform((p) => cleanPhoneNumber(p)),
+  password: z.string().min(8, "Password must contain at least 8 characters."),
 });
-
 export const loginUserSchema = z.object({
-    email: z.string().email("Invalid email address."),
-    password: z.string().min(1, "Password is required.")
-})
+  email: z.string().email("Invalid email address."),
+  password: z.string().min(1, "Password is required."),
+});
