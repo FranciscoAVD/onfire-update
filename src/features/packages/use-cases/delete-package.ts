0a37238f4a5d563c702fefd0ea1c0a4041eedd3a
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-15 16:03:58 -0400

    added delete util

---
+++ src/features/packages/use-cases/delete-package.ts
@@ -0,0 +1,12 @@
+import { db } from "@/database";
+import { packageTable } from "@/database/schema";
+import { Package } from "@/database/types";
+import { eq } from "drizzle-orm";
+
+export async function deletePackage(id: Package["id"]):Promise<void>{
+  try{
+    await db.delete(packageTable).where(eq(packageTable.id, id));
+  }catch(err){
+    throw new Error("Database error while deleting package.");
+  }
+}
