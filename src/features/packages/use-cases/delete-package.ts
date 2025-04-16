248797153551a9118d38cde83ae6acf8eca372dd
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-16 12:07:46 -0400

    formatted

--- src/features/packages/use-cases/delete-package.ts
+++ src/features/packages/use-cases/delete-package.ts
@@ -3,10 +3,10 @@ import { packageTable } from "@/database/schema";
 import { Package } from "@/database/types";
 import { eq } from "drizzle-orm";
 
-export async function deletePackage(id: Package["id"]):Promise<void>{
-  try{
+export async function deletePackage(id: Package["id"]): Promise<void> {
+  try {
     await db.delete(packageTable).where(eq(packageTable.id, id));
-  }catch(err){
+  } catch (err) {
     throw new Error("Database error while deleting package.");
   }
 }
