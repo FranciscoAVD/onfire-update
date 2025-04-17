93c13f131408719a85256ec53617b93b97ca7719
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-17 17:09:42 -0400

    added active package card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function DashboardPage(){
-    return <main>Dashboard</main>
+import { Main } from "@/components/ui/main";
+import { getCurrentUser } from "@/features/auth/lib/session";
+import { UserPackageCard } from "@/features/packages/components/user-package-card";
+import { getActivePurchase } from "@/features/purchases/use-cases/get-purchase";
+
+export default async function DashboardPage() {
+  const user = await getCurrentUser();
+  if (!user) return null;
+  //get active user package
+  const purchase = await getActivePurchase(user.getId());
+
+  return (
+    <Main>
+      <section className="grid grid-cols-2 gap-6">
+        {purchase && <UserPackageCard p={purchase} />}
+      </section>
+    </Main>
+  );
 }
