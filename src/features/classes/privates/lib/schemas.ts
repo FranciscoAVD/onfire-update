4c9983543ae30d1a4971e6db26c768300824b437
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-04-24 09:42:38 -0400

    adjusted to fit api version and not form data

--- src/features/classes/privates/lib/schemas.ts
+++ src/features/classes/privates/lib/schemas.ts
@@ -1,41 +1,36 @@
-import { getDay, isValid, isWeekend, parseISO } from "date-fns";
+import { getDay, isValid, parseISO, toDate } from "date-fns";
 import { z } from "zod";
 import { rhythmSchema } from "@/features/classes/lib/schemas";
-import { weekPrivateTimeSlots } from "@/features/classes/lib/constants";
+import {
+  DISABLED_DAYS,
+  weekPrivateTimeSlots,
+} from "@/features/classes/lib/constants";
 
 export const addPrivateSchema = z
   .object({
     date: z
       .string()
-      .min(10, "Invalid date.")
-      .refine((d) => isValid(parseISO(d)), "Invalid date.")
+      .refine(
+        (d) =>
+          isValid(parseISO(d)) || !DISABLED_DAYS.includes(getDay(parseISO(d))),
+        {
+          message: "Invalid date.",
+        },
+      )
       .transform((d) => parseISO(d)),
-    time: z
-      .string()
-      .min(4, "Invalid time.")
-      .refine((t) => Number.isInteger(+t))
-      .transform((t) => Number(t)),
+    time: z.number(),
     rhythm: rhythmSchema,
   })
   .superRefine((data, ctx) => {
     const timeMap = weekPrivateTimeSlots.get(getDay(data.date));
-    if (!timeMap) {
+    if (!timeMap || !timeMap.has(data.time)) {
       ctx.addIssue({
-        path: ["date"],
-        message: "Invalid date.",
         code: z.ZodIssueCode.custom,
-        fatal: true,
-      });
-      return z.NEVER;
-    }
-    if (!timeMap.has(data.time)) {
-      ctx.addIssue({
-        path: ["time"],
         message: "Invalid time.",
-        code: z.ZodIssueCode.custom,
         fatal: true,
       });
       return z.NEVER;
     }
   });
+
 export const statusSchema = z.enum(["pending", "ctl", "no-show", "taken"]);
